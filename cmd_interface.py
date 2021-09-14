460404d7fd3ad71bf05a05eadc75bc186d09c197
Hew Boland <hewbolan@umich.edu>
2021-09-14 15:12:05 -0400

    added support for messages

--- cmd_interface.py
+++ cmd_interface.py
@@ -5,7 +5,41 @@ while 1:
   # Poll keyboard
   if msvcrt.kbhit():
     key = msvcrt.getch()
-  if key == 'f':
-    ser.write('C21FE')
-  elif key == 's':
-    ser.write('C21SE')
+    print(key)
+    # stop
+    if key == ' ':
+      ser.write('C21SE')
+
+    # forward
+    elif key == 'w':
+      ser.write('C21SE')
+
+    # backwards
+    elif key == 's':
+      ser.write('C21BE')
+    # left
+    elif key == 'a':
+      ser.write('C21LE')
+
+    # right
+    elif key == 'd':
+      ser.write('C21RE')
+
+    # speed up
+    elif key == ']':
+      ser.write('C21AE')
+
+    # slow down
+    elif key == '[':
+      ser.write('C21DE')
+
+    # message for display
+    elif key == '`':
+        message = input("type message: ")
+        if len(message) > 4:
+          print("message supplied is too long must be < 4 chars)")
+        else:
+          total_len = len(message) + 1
+          val = "C" + char(total_len) + "4" + message + "E"
+          print(val) # just for testing
+          ser.write(val)
